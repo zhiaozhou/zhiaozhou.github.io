cec9623c9658ee0e83b848ecf8f57958329c4502
Zhiao Zhou <zz1749@nyu.edu>
2018-05-12 17:48:22 -0400

    'update'

--- travel/org-locations.js
+++ travel/org-locations.js
@@ -1,22 +1,192 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Singapore, Singapore. 2004",
+    1.293089,
+    103.851927
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Eugene, Oregon, USA. Februrary 2008.",
+    44.052069,
+    -123.086754
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Los Angeles, California, USA. August 2013",
+    34.052234,
+    -118.243685
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Las Vages, Nevada, USA. August 2013",
+    36.169941,
+    -115.139830
+  ],
+  [
+    "Honululu, Hawaii, USA. January 2014",
+    21.306944,
+    -157.858333
+  ],
+  [
+    "Cambridge, UK. August 2014",
+    52.205337,
+    0.121817
+  ],
+  [
+    "London, UK. August 2014",
+    51.507351,
+    -0.127758
+  ],
+  [
+    "St. Andrews, UK. August 2014",
+    56.339775,
+    -2.796721
+  ],
+  [
+    "Edinburgh, UK. August 2014",
+    55.953252,
+    -3.188267
+  ],
+  [
+    "Milan, Italy. July 2015.",
+    45.464204,
+    9.189982
+  ],
+  [
+    "Genova, Italy. July 2015.",
+    44.405650,
+    8.946256
+  ],
+  [
+    "Venice, Italy. July 2015.",
+    45.440847,
+    12.315515
+  ],
+  [
+    "Montpellier, France. August 2015.",
+    43.608773,
+    3.879636
+  ],
+  [
+    "Bordeaux, France. September 2015.",
+    44.837789,
+    -0.579180
+  ],
+  [
+    "Carcassonne, France. September 2015.",
+    43.212161,
+    2.353663
+  ],
+  [
+    "Toulouse, France. Octorbor 2015.",
+    43.604652,
+    1.444209
+  ],  
+  [
+    "Brittany, France. Octorbor 2015.",
+    48.202047,
+    -2.932644
+  ],  
+  [
+    "Lourdes, France. Octorbor 2015.",
+    43.091463,
+    -0.045726
+  ],  
+  [
+    "Reykjavik, Iceland. December 2015.",
+    64.126521,
+    -21.817439
+  ],  
+  [
+    "Oslo, Norway. December 2015.",
+    59.913869,
+    10.752245
+  ],  
+  [
+    "Bergen, Norway. December 2015.",
+    60.391263,
+    5.322054
+  ],  
+  [
+    "Stockholm, Sweden. January 2016.",
+    59.329323,
+    18.068581
+  ],  
+  [
+    "Rovaniemi, Finland. January 2016.",
+    66.503948,
+    25.729391
+  ],  
+  [
+    "Prague, Czech. January 2016.",
+    50.075538,
+    14.437800
+  ],  
+  [
+    "Cesky Krumlov, Czech. January 2016.",
+    48.812735,
+    14.317466
+  ],  
+  [
+    "Budapest, Hungary. January 2016.",
+    47.497912,
+    19.040235
+  ],  
+  [
+    "Innsbruck, Austria. January 2016.",
+    47.269212,
+    11.404102
+  ],  
+  [
+    "Hallstatt, Austria. January 2016.",
+    47.562234,
+    13.649262
+  ], 
+  [
+    "Lucerne, Switzerland. January 2016.",
+    47.050168,
+    8.309307
+  ], 
+  [
+    "Munich, Germany. January 2016.",
+    48.135125,
+    11.581980
+  ], 
+  [
+    "Amsterdam, Netherland. January 2016.",
+    52.370216,
+    4.895168
+  ], 
+  [
+    "Rotterdam, Netherland. January 2016.",
+    51.924420,
+    4.477733
+  ], 
+  [
+    "Tokyo, Japan. Octobor 2016.",
+    35.689487,
+    139.691706
+  ], 
+  [
+    "Port Douglas, Australia. January 2017.",
+    -16.483611,
+    145.465278
+  ], 
+  [
+    "Cairns, Australia. January 2017.",
+    -16.918551
+    145.778055
+  ], 
+  [
+    "Vancouver, Canada. August 2017.",
+    49.282729,
+    -123.120738
+  ], 
+  [
+    "Banff, Canada. August 2017.",
+    51.178363,
+    -115.570769
+  ], 
+  [
+    "New York, USA. August 2017.",
+    40.712775,
+    -74.005973
   ]
 ];
